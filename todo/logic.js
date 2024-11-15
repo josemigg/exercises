fe95cb32964f295b214b1a223979565a4984c2c9
Jose Miguel Gonzalez Garcia <josemigonzalez@metricool.com>
2024-11-15 09:40:04 +0100

    Added logic exercises

---
+++ todo/logic.js
@@ -0,0 +1,120 @@
+/*
+Escribe qué valor debe ser X e Y para que las siguientes condiciones se cumplan (den true)
+Cada ejercicio puede tenenr un valor de X e y distinto
+Es posible que alguna condición no se pueda dar nunca. Indica por qué
+*/
+let result;
+let x;
+let;
+
+// Ejemplo, x= 3, y = 1
+result = x > y;
+console.log(result);
+
+//
+result = x > y + 5;
+console.log(result);
+
+//
+result = 'pepito'.length === y;
+console.log(result);
+
+//
+result = 'La venganza de los ornitorrincos salvajes'.length < y + 10 && x.length === y;
+console.log(result);
+
+//
+result = x >= y;
+console.log(result);
+
+//
+result = x !== 'number';
+console.log(result);
+
+//
+result = y && !x;
+console.log(result);
+
+//
+result = y <= x || x > y;
+console.log(result);
+
+//
+result = -1 + x === y - 25;
+console.log(result);
+
+//
+result = typeof y === 'String';
+console.log(result);
+
+//
+result = x > 0 && x < 1 && y > 0 && y < x;
+console.log(result);
+
+//
+result = x !== '' && x.length < 2;
+console.log(result);
+
+//
+result = typeof x === 'number' && x > '5';
+console.log(result);
+
+//
+result = x.length > 5 && x.length < 6.0000000000001;
+console.log(result);
+
+//
+result = y.length && x.length && x.length !== y.length;
+console.log(result);
+
+//
+result = x.length > 0 && x.length < 1;
+console.log(result);
+
+//
+result = typeof x === 'boolean' && typeof y === 'string';
+console.log(result);
+
+//
+result = x !== y && x === 5 && y == 5;
+console.log(result);
+
+//
+result = !x || (x && !y) || (y && x);
+console.log(result);
+
+//
+result = y === x && y !== 'number' && x.length;
+console.log(result);
+
+//
+result = (!x && !y && x) || (y && x && x < 100 && y < 0);
+console.log(result);
+
+//
+result = y !== '' && typeof y === 'number';
+console.log(result);
+
+//
+result = y.toLowerCase().includes('A');
+console.log(result);
+
+//
+result = y === '' && typeof y !== 'number';
+console.log(result);
+
+//
+result = !y && y !== false && y !== 0;
+console.log(result);
+
+//
+result = typeof x == 'number' && !x;
+console.log(result);
+
+//
+result = (x === y && x < 1 && y > 0) || (x === y && x > 1 && y < 0) || (x !== y && x === y + 1) || x == 5;
+console.log(result);
+
+//
+result = x > 5 && y.length * 2 == x;
+console.log(result);
