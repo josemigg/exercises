94395ecdbcad0e66145adc795067ed3716b0132d
Jose Miguel Gonzalez Garcia <josemigonzalez@metricool.com>
2024-11-27 10:51:38 +0100

    Ejercicios array y objetos básicos

---
+++ todo/arrays-basic.js
@@ -0,0 +1,32 @@
+/*
+  1. Crea un array que contenga un listado de tareas. Las tareas tienen un texto y pueden estar completadas o pendientes. 
+*/
+
+/*
+  2. Crea un array que contenga un listado de vehículos. Usa al menos 5 propiedades en cada vehículo. 
+*/
+
+/*
+  3. Imagina que estás en un supermercado haciendo la compra y tienes que comprar lo que está 
+  en la variable shoppingList.
+
+  Crea una función a la que le pases dos parámetros, el array de la compra, y el elemento que has comprado
+  esa función debe buscar el elemento en el array y eliminarlo.
+
+  Por ejemplo, si le paso un array ['pepino', 'tomate', 'sandía'] y el string 'tomate', debe devolver un array ['pepino', 'sandía']
+*/
+const shoppingList = [
+  'x2 leche de coco',
+  'pimiento rojo',
+  'pimiento verde',
+  'x6 garrafas de agua',
+  'aguacates',
+  'avena',
+  'tomate frito',
+  'pepino'
+];
+
+/**
+ * 4. Repite el ejercicio anterior sin modificar el array que le pasas como parámetro a la función.
+ * Si ya has hecho el ejercicio anterior sin modificar el array, hazlo modificándolo.
+ */
