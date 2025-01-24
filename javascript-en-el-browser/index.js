1415721f0ab638135858a425603efca674ff7d49
Jose Miguel Gonzalez Garcia <josemigonzalez@metricool.com>
2025-01-24 19:42:54 +0100

    Added render perricos

---
+++ javascript-en-el-browser/index.js
@@ -0,0 +1,52 @@
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function generateRandomTask() {
+  return {
+    text: `Texto aleatorio número ${getRandomInt(1, 1000)}`,
+    isCompleted: getRandomInt(0, 1) === 1
+  };
+}
+
+function getRandomArray() {
+  const randomTasks = [];
+  for (let i = 0; i < 10; i++) {
+    randomTasks.push(generateRandomTask());
+  }
+  return randomTasks;
+}
+
+// Estas funciones serán las que iremos cambiando con los ejemplos
+function regenerateArray() {
+  const tasks = getRandomArray();
+  let newTasksHTML = '';
+
+  tasks.forEach((task) => {
+    newTasksHTML += `
+     <div class="task">
+        <span class="${task.isCompleted ? 'completed' : ''}">${task.text}</span> -
+        <span class="status">${task.isCompleted ? 'completed' : 'pending'}</span>
+      </div>`;
+  });
+  document.querySelector('#tasks').innerHTML = newTasksHTML;
+}
+
+function addFirst() {}
+
+function addLast() {}
+
+// event listeners para que los botones llamen a las funciones anteriores
+document.querySelector('#regenate').addEventListener('click', () => {
+  regenerateArray();
+});
+
+document.querySelector('#add-first').addEventListener('click', () => {
+  addFirst();
+});
+
+document.querySelector('#add-last').addEventListener('click', () => {
+  addLast();
+});
--- javascript-en-el-browser/random-dogs/index.js
+++ javascript-en-el-browser/random-dogs/index.js
@@ -1,5 +1,7 @@
 const perricosArray = [
   'https://images.dog.ceo/breeds/affenpinscher/n02110627_10439.jpg',
+  'https://images.dog.ceo/breeds/affenpinscher/n02110627_10439.jpg',
+  'https://images.dog.ceo/breeds/affenpinscher/n02110627_10439.jpg',
   'https://images.dog.ceo/breeds/affenpinscher/n02110627_10439.jpg'
 ];
 console.log(perricosArray);
@@ -8,33 +10,56 @@ console.log(perricosArray);
 
 // addPerrico();
 
+function addSocialListeners() {
+  document.querySelectorAll('.like').forEach((buttonNode) => {
+    buttonNode.addEventListener('click', function () {
+      console.log('me gusta clickado');
+      const hermanico = buttonNode.previousElementSibling;
+      const likeCountNode = hermanico.querySelector('.like-count');
+      likeCountNode.innerText = Number(likeCountNode.innerText) + 1;
+    });
+  });
+}
+
+function renderPerrico(dogImage) {
+  const htmlAdd = `<div class="card">
+  <img src="${dogImage}" alt="Perro" />
+  <br />
+  <p><span class="like-count"></span>❤️ <span class="dislike-count"></span>🤮</p>
+  <button class="like">Preciosísimo</button> <button class="dislike">Feísisimo</button>
+</div>`;
+  document.querySelector('#dog-list').innerHTML += htmlAdd;
+}
+
 function renderPerricoArray() {
   const dogList = document.querySelector('#dog-list');
   dogList.innerHTML = '';
 
-  perricosArray.forEach((dogImage, index) => {
-    const htmlAdd = `<div class="card">
-  <img src="${dogImage}" alt="Perro" />
-  <br />
-  <p>❤️ 🤮</p>
-  <button>Preciosísimo</button> <button>Feísisimo</button>
-</div>`;
-
-    console.log('innerHtml posición', index, dogList.innerHTML);
-
-    dogList.innerHTML += htmlAdd;
+  perricosArray.forEach((dogImage) => {
+    renderPerrico(dogImage);
   });
+
+  addSocialListeners();
 }
 
 const addPerrico = async () => {
   const perricoImg = await getRandomDogImage();
   console.log(perricoImg);
   perricosArray.push(perricoImg);
-  renderPerricoArray();
+  renderPerrico(perricoImg);
+  addSocialListeners();
 };
-
-renderPerricoArray();
 
 document.querySelector('#add-1-perrico').addEventListener('click', function () {
   addPerrico();
 });
+
+document.querySelector('#add-5-perricos').addEventListener('click', function () {
+  addPerrico();
+  addPerrico();
+  addPerrico();
+  addPerrico();
+  addPerrico();
+});
+
+renderPerricoArray();
