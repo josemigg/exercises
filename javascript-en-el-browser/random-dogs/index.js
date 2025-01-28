0baf49a656cb9a3291db0a18a7e8abda19719d29
Jose Miguel Gonzalez Garcia <josemigonzalez@metricool.com>
2025-01-28 18:25:20 +0100

    Updated dogs with like-filter

--- javascript-en-el-browser/random-dogs/index.js
+++ javascript-en-el-browser/random-dogs/index.js
@@ -1,16 +1,27 @@
-const perricosArray = [
-  'https://images.dog.ceo/breeds/affenpinscher/n02110627_10439.jpg',
-  'https://images.dog.ceo/breeds/affenpinscher/n02110627_10439.jpg',
-  'https://images.dog.ceo/breeds/affenpinscher/n02110627_10439.jpg',
-  'https://images.dog.ceo/breeds/affenpinscher/n02110627_10439.jpg'
-];
+const perricosArray = ['https://images.dog.ceo/breeds/affenpinscher/n02110627_10439.jpg'];
 console.log(perricosArray);
 
 // console.log(getRandomDogImage());
 
 // addPerrico();
 
-function addSocialListeners() {
+function renderPerrico() {}
+
+function renderPerricoArray() {
+  const dogList = document.querySelector('#dog-list');
+  dogList.innerHTML = '';
+
+  perricosArray.forEach((dogImage, index) => {
+    const htmlAdd = `<div class="card">
+  <img src="${dogImage}" alt="Perro" />
+  <br />
+  <p><span class="like-count"></span>❤️ <span class="dislike-count"></span>🤮</p>
+  <button class="like">Preciosísimo</button> <button class="dislike">Feísisimo</button>
+</div>`;
+
+    dogList.innerHTML += htmlAdd;
+  });
+
   document.querySelectorAll('.like').forEach((buttonNode) => {
     buttonNode.addEventListener('click', function () {
       console.log('me gusta clickado');
@@ -21,37 +32,45 @@ function addSocialListeners() {
   });
 }
 
-function renderPerrico(dogImage) {
+const addPerrico = async (addToStart) => {
+  const perricoImg = await getRandomDogImage();
+
+  if (addToStart) {
+    perricosArray.unshift(perricoImg);
+  } else {
+    perricosArray.push(perricoImg);
+  }
+
+  const dogList = document.querySelector('#dog-list');
+
   const htmlAdd = `<div class="card">
-  <img src="${dogImage}" alt="Perro" />
+  <img src="${perricoImg}" alt="Perro" />
   <br />
   <p><span class="like-count"></span>❤️ <span class="dislike-count"></span>🤮</p>
   <button class="like">Preciosísimo</button> <button class="dislike">Feísisimo</button>
 </div>`;
-  document.querySelector('#dog-list').innerHTML += htmlAdd;
-}
 
-function renderPerricoArray() {
-  const dogList = document.querySelector('#dog-list');
-  dogList.innerHTML = '';
+  if (addToStart) {
+    dogList.innerHTML = htmlAdd + dogList.innerHTML;
+  } else {
+    dogList.innerHTML = dogList.innerHTML + htmlAdd;
+  }
 
-  perricosArray.forEach((dogImage) => {
-    renderPerrico(dogImage);
+  document.querySelectorAll('.like').forEach((buttonNode) => {
+    buttonNode.addEventListener('click', function () {
+      const hermanico = buttonNode.previousElementSibling;
+      const likeCountNode = hermanico.querySelector('.like-count');
+      likeCountNode.innerText = Number(likeCountNode.innerText) + 1;
+    });
   });
-
-  addSocialListeners();
-}
-
-const addPerrico = async () => {
-  const perricoImg = await getRandomDogImage();
-  console.log(perricoImg);
-  perricosArray.push(perricoImg);
-  renderPerrico(perricoImg);
-  addSocialListeners();
 };
 
 document.querySelector('#add-1-perrico').addEventListener('click', function () {
   addPerrico();
+});
+
+document.querySelector('#add-1-perrico-start').addEventListener('click', function () {
+  addPerrico(true);
 });
 
 document.querySelector('#add-5-perricos').addEventListener('click', function () {
@@ -60,6 +79,19 @@ document.querySelector('#add-5-perricos').addEventListener('click', function () 
   addPerrico();
   addPerrico();
   addPerrico();
+});
+
+document.querySelector('#like-filter').addEventListener('click', function () {
+  document.querySelectorAll('.like-count').forEach((likeCountNode) => {
+    if (likeCountNode.innerText === '') {
+      const cardNode = likeCountNode.closest('.card');
+      cardNode.style.display = 'none';
+    }
+  });
+});
+
+document.querySelector('#dislike-filter').addEventListener('click', function () {
+  console.log('dislike filter clicked');
 });
 
 renderPerricoArray();
